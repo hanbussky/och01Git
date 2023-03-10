ebfb17863dad10b54a176192319f4a5a052d9f32
user <user@DESKTOP-2PGRMF4>
2023-03-10 10:47:41 +0900

    1. webapp의 html 2. och01 package의 Java

---
+++ och01Git/src/main/java/och01/Add3.java
@@ -0,0 +1,61 @@
+package och01;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Add3
+ */
+@WebServlet("/Add3")
+public class Add3 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Add3() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// 목표 : 1부터 누적값 전달
+		int num = Integer.parseInt(request.getParameter("num"));
+		String loc = request.getParameter("loc");
+		System.out.println("Add3 num->"+num);
+		int sum = 0;
+		for (int i = 1; i < num; i++) {
+			sum += i;
+		}
+		System.out.println("Add3 sum->"+sum);
+		
+		request.setCharacterEncoding("utf-8");
+		response.setContentType("text/html;charset=utf-8");
+		// 공식 -->사용자 Browser에 보여주는 객체
+		PrintWriter out = response.getWriter();
+		out.println("<html><body>");
+		out.printf("<h1>1부터 %d까지 합계</h1>",num);
+		out.printf("<h4>:PC--> %s</h4>",loc);
+		out.println(sum);
+		out.println("</body></html>");
+		out.close();
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ och01Git/src/main/java/och01/GuGu.java
@@ -0,0 +1,52 @@
+package och01;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class GuGu
+ */
+//@WebServlet("/GuGu")  // web.xml 때문에 주석
+public class GuGu extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public GuGu() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// 1. Parameter ->num
+		// 2. 구구단 출력
+		int num = Integer.parseInt(request.getParameter("num"));
+		response.setContentType("text/html;charset=utf-8");
+		PrintWriter out = response.getWriter();
+		out.println("<html><body>");
+		out.printf("<h1>구구단"+num+"단</h1>");
+		for (int i = 1; i <= 9; i++) {
+				out.printf("%d * %d = %d<br>", num, i, (num*i));
+		}
+		out.println("</body></html>");
+		out.close();
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
